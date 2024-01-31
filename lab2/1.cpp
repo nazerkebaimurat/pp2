89a201646cc7cf647d5ebed3244ac4e8be778324
nazerkebaimurat <157196449+nazerkebaimurat@users.noreply.github.com>
2024-01-31 19:11:43 +0600

    Update 1.cpp

--- lab2/1.cpp
+++ lab2/1.cpp
@@ -1,16 +1,1 @@
-#include <iostream>
-#include <bitset>
 
-using namespace std;
-
-int main(){
-    int n;
-    cin >> n;
-
-    bitset <8> bit(n);
-
-    bit.to_string = num;
-
-    cout << num;
-return 0;
-}
